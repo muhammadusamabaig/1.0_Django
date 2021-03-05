afe0b7bf32317a1a5b518e74288c371b09936d8c
Muhammad Usama Baig <muhammadusamabaig099@gmail.com>
2021-03-05 21:07:13 +0500

    ok

--- CV/settings.py
+++ CV/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
-import django_heroku
 
 import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -125,4 +124,3 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT=os.path.join(BASE_DIR,"staticfiles")
-django_heroku.settings(locals())
